3f3db5717e79c8f977694ed16bf402d664828329
Gabriel Torregrosa <g.torregrosa@outlook.com>
2023-01-06 19:23:42 +0100

    Add plots computing the artificial deconvolution

--- examples/paper/artificialDistributionsFiniteMixtures.jl
+++ examples/paper/artificialDistributionsFiniteMixtures.jl
@@ -9,37 +9,8 @@ using CSV
 dNoise = Dict()
 dNoise["Normal"] = Normal(0,1)
 dNoise["SkewNormal"] = SkewNormal(-1.32,1.65,7)
-# dNoise["BimodalAsym"] = MixtureModel([
-#                         Normal(-0.82-0.16,0.41),
-#                         Normal(0.82-0.16,0.71),
-#                         ],[.4,.6])
-# dNoise["BimodalSym"] = MixtureModel([
-#                         Normal(-0.9,0.5),
-#                         Normal(0.9,0.5),
-#                         ],[.5,.5])
-# dNoise["TrimodalAsym"] = MixtureModel([
-#                         Normal(-1.7+.08,0.65),
-#                         Normal(0+.08,0.35),
-#                         Normal(1.3+.08,0.35),
-#                         ],[.2,.6,.2])
-# dNoise["TrimodalSym"] = MixtureModel([
-#                         Normal(-1.3,0.65),
-#                         Normal(0,0.15),
-#                         Normal(1.3,0.65),
-#                         ],[.25,.5,.25])
 dNoise["Student"] = TDist(3) 
-# dNoise["Binomial"] = MixtureModel(
-#     Normal[
-#         Normal(-2,1),
-#         Normal(6,1)
-#     ],
-#     [.5,.5]
-#     )
-# dNoise["Laplace"] = Laplace(0,.72) 
-# dNoise["LaplaceSharp"] = MixtureModel([
-#                         Laplace(0.,0.2),
-#                         Laplace(0.,0.1),
-#                         ],[.5,.5]);
+
 
 dTarget = Dict()
 dTarget["BimodalAsym"] = MixtureModel([
@@ -52,74 +23,21 @@ dTarget["BimodalSym"] = MixtureModel([
                         ],[.5,.5])
 dTarget["SkewNormal"] = SkewNormal(-1.32,1.65,7)
 
-# Plot noise distributions
-x = -10:.1:10
-l = []
-for (i,j) in pairs(dNoise)
-    push!(l,plot(x,pdf.(j,x),title=i,linewidth=3,label=""))
-end
-fig = plot(l...,layout=(1,3),size=[1000,200])
-savefig(fig,"Plots/Finite_Noise.pdf")
-
-# Plot target distributions
-x = -5:.1:5
-l = []
-for (i,j) in pairs(dTarget)
-    push!(l,plot(x,pdf.(j,x),title=i,linewidth=3,label=""))
-end
-fig = plot(l...,layout=(1,3),size=[1000,330])
-savefig(fig,"Plots/Finite_Target.pdf")
-
-# Plot convolution distributions
-x = -4:.1:4
-bins=-4:.2:4
-plots = []
-snr=4
-push!(plots,plot(ticks=nothing,axis=false))
-for (i,dn) in pairs(dNoise)
-
-    push!(plots,plot(x,pdf.(dn,x),title=i,linewidth=3,label=""))
-
-end
-for (j,dt) in pairs(dTarget)
-
-    push!(plots,plot(x,pdf.(dt,x),title=j,linewidth=3,label=""))
-
-    for (i,dn) in pairs(dNoise)
-
-        data = rand(dn,10000)/snr+rand(dt,10000)
-        push!(plots,histogram(data,bins=bins,label=""))
-
-    end
-
-end
-fig = plot(plots...,layout=(4,4),size=(1000,800))
-savefig(fig,"Plots/Finite_Convolution.pdf")
-
 # Compute statistics
 x = -10:.1:10
 
-resultsNoise = 0
-try
-    global resultsNoise = CSV.read("Tables/Finite_Noise.csv",DataFrame)
-catch
-    global resultsNoise = DataFrame(:Noise=>String[],:Ratio=>Float64[],:Size=>Float64[],:MIO_mean=>Float64[],:MIO_std=>Float64[])
-end
+resultsNoise = DataFrame(:Noise=>String[],:Ratio=>Float64[],:Size=>Float64[],:MIO_mean=>Float64[],:MIO_std=>Float64[])
 
-resultsDeconvolution = 0
-try
-    global resultsDeconvolution = CSV.read("Tables/Finite_Deconvolution.csv",DataFrame)
-catch
-    global resultsDeconvolution = DataFrame(:Noise=>String[],:Target=>String[],:Ratio=>Float64[],:Size=>Float64[],:MIO_mean=>Float64[],:MIO_std=>Float64[],
+resultsDeconvolution = DataFrame(:Noise=>String[],:Target=>String[],:Ratio=>Float64[],:Size=>Float64[],:MIO_mean=>Float64[],:MIO_std=>Float64[],
     :MIO_Convolution_mean=>Float64[],:MIO_Convolution_std=>Float64[],:MIO_Neumann=>Float64[])
-end
 
-mioNeumann(x,y,dt) = 1-sum(abs.(pdf.(dt,x).-y)*(x[2]-x[1]))/2
 for N in [100,1000,10000]
+    println(N)
     for snr in [10,1]
         for (i,dn) in pairs(dNoise)
 
             n = reshape(rand(dn,N)/snr,N,1)
+            fitNoise = 0
             fitNoise = finiteGaussianMixture(n,k=4,κ0=0.001)
 
             if snr == 1 && (size(resultsNoise[(resultsNoise[!,"Size"].==N) .& (resultsNoise[!,"Noise"].==i),:])[1] == 0)
@@ -144,20 +62,57 @@ for N in [100,1000,10000]
                     
                     ft(x) = pdf(dt,x[1])
 
-                    fitDeconvolution = finiteGaussianMixtureDeconvolution(c,fitNoise,k=4)
+                    @time fitDeconvolution = finiteGaussianMixtureDeconvolution(c,fitNoise,k=4)
                     mios = scBayesDeconv.metrics.mio(fitDeconvolution,ft)
 
-                    fitConvolution = infiniteGaussianMixture(c,k=1)
+                    fitConvolution = finiteGaussianMixture(c,k=4)
                     miosConvolution = scBayesDeconv.metrics.mio(fitConvolution,ft)
 
-                    yNeumann, xNeumann = neumannDeconvolution(n,c)
-                    mioneumann = mioNeumann(xNeumann,yNeumann,dt)
+                    dNewmann = neumannDeconvolution(n,c)
+                    mioneumann = scBayesDeconv.metrics.mio(dNewmann,ft)
 
                     append!(resultsDeconvolution,
                         ["Noise"=>i,"Target"=>j,"Ratio"=>snr,"Size"=>N,"MIO_mean"=>round(mean(mios),digits=4),"MIO_std"=>round(std(mios),digits=4),
                         "MIO_Convolution_mean"=>round(mean(miosConvolution),digits=4),"MIO_Convolution_std"=>round(std(miosConvolution),digits=4),"MIO_Neumann"=>round(mioneumann,digits=4)]
                     )
                     CSV.write("Tables/Finite_Deconvolution.csv", string.(resultsDeconvolution))
+
+                    #Plots
+                    local x = -4:.01:4.5
+                    xx = reshape(x,1,length(x))
+                    
+                    p1 = histogram(c,bins=-4:.1:5,title="Convolution",label="",normalize=true,color="green",ylabel="p(x)",xlabel="x")
+                    plot!(p1,x,pdf(sample(fitDeconvolution,distribution=:Convolution),xx),color="red",alpha=0.2,label="BD samples")
+                    for i in 2:100
+                        plot!(p1,x,pdf(sample(fitDeconvolution,distribution=:Convolution),xx),label="",color="red",alpha=0.1)
+                    end
+                    for i in 1:100
+                        plot!(p1,x,pdf(sample(fitConvolution),xx),label="",color="green",alpha=0.1)
+                    end
+                    
+                    p4 = histogram(n,bins=-4:.1:5,title="Noise",label="",normalize=true,color="green",ylabel="p(x)",xlabel="x")
+                    plot!(p4,x,pdf(sample(fitNoise),xx),color="red",alpha=0.2,label="BD samples")
+                    for i in 1:100
+                        plot!(p4,x,pdf(sample(fitNoise),xx),label="",color="red",alpha=0.1)
+                    end
+
+                    p2 = histogram(t,bins=-4:.1:4,title="Bayesian deconvolution",normalize=true,color="lightblue",ylabel="p(x)",xlabel="x",label="Deconv. sample")
+                    y = zeros(100,length(x))
+                    plot!(p2,x,pdf(sample(fitDeconvolution),xx),color="red",alpha=0.2,label="BD samples")
+                    for i in 1:100
+                        plot!(p2,x,pdf(sample(fitDeconvolution),xx),label="",color="red",alpha=0.1)
+                    end
+                    plot!(p2,x,mean(y,dims=1)[1,:],label="Bayesian deconv.",color="black",legendfontsize=8)
+                    xlims!(-4,4)
+                    ylims!(0,.75)
+                    
+                    p3 = histogram(t,bins=-4:.1:4,title="FFT deconvolution",normalize=true,color="lightblue",ylabel="p(x)",xlabel="x",label="Deconv. sample")
+                    plot!(p3,x,dNewmann(Vector(x)),linewidth=2,label="FFT decon.")
+                    xlims!(-4,4)
+                    ylims!(0,.75)
+
+                    plot(p1,p4,p2,p3,layout=(1,4),size=[1400,220])
+                    savefig(string("Plots/Finite_artificial_plots/",i,"_",j,"_",snr,"_",N,".pdf"))
 
                 end
 
